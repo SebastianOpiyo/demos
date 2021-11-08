1c9cac366702b2bfa1fa20d8a86d9b983a5fd30d
SebastianOpiyo <sebamopale@gmail.com>
2021-11-08 20:34:01 +0300

    Add a working face recognizer.

--- visualRecognitionApp/src/faces.py
+++ visualRecognitionApp/src/faces.py
@@ -3,14 +3,14 @@ import cv2
 import pickle
 
 face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
-eye_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_eye.xml')
-smile_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_smile.xml')
+# eye_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_eye.xml')
+# smile_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_smile.xml')
 
 
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 recognizer.read("./recognizers/face-trainner.yml")
 
-labels = {"person_name": 1}
+# labels = {"person_name": 1}
 with open("pickles/face-labels.pickle", 'rb') as f:
     og_labels = pickle.load(f)
     labels = {v:k for k,v in og_labels.items()}
@@ -23,15 +23,15 @@ while(True):
     gray  = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
     for (x, y, w, h) in faces:
-        #print(x,y,w,h)
+        print(x,y,w,h)
         roi_gray = gray[y:y+h, x:x+w] #(ycord_start, ycord_end)
         roi_color = frame[y:y+h, x:x+w]
 
         # recognize? deep learned model predict keras tensorflow pytorch scikit learn
         id_, conf = recognizer.predict(roi_gray)
         if conf>=4 and conf <= 85:
-            #print(5: #id_)
-            #print(labels[id_])
+            # print(5: #id_)
+            print(labels[id_])
             font = cv2.FONT_HERSHEY_SIMPLEX
             name = labels[id_]
             color = (255, 255, 255)
