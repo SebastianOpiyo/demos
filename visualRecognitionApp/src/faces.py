b018f58349a650058b332962ec611b7140105f84
Sebastian A. Opiyo <saopiyo@usiu.ac.ke>
2022-03-07 18:19:34 +0300

    Add image capture module and and alternative trainer.

--- visualRecognitionApp/src/faces.py
+++ visualRecognitionApp/src/faces.py
@@ -1,9 +1,10 @@
-
 import numpy as np
 import cv2
 import pickle
 import os
 import face_recognition
+
+from src.helpers import rescale_frame
 
 face_cascade = cv2.CascadeClassifier('./cascades/data/haarcascade_frontalface_alt2.xml')
 # eye_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_eye.xml')
@@ -13,52 +14,52 @@ face_cascade = cv2.CascadeClassifier('./cascades/data/haarcascade_frontalface_al
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 recognizer.read("./recognizers/face-trainner.yml")
 
-# labels = {"person_name": 1}
+labels = {"person_name": 1}
 with open("pickles/face-labels.pickle", 'rb') as f:
     og_labels = pickle.load(f)
-    labels = {v:k for k,v in og_labels.items()}
+    labels = {v: k for k, v in og_labels.items()}
 
 cap = cv2.VideoCapture(0)
 
 # call one of the resolution settings right here.
-while(True):
+while (True):
     # Capture frame-by-frame
     ret, frame = cap.read()
     # Change image resolution by using helper methods defined in the helper module.
     # frame = rescale_frame(frame, percent=70)
     # cv2.imshow('resized_frame', frame)
-    gray  = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
 
     for (x, y, w, h) in faces:
-        print(x,y,w,h)
-        roi_gray = gray[y:y+h, x:x+w] #(ycord_start, ycord_end)
-        roi_color = frame[y:y+h, x:x+w]
+        print(x, y, w, h)
+        roi_gray = gray[y:y + h, x:x + w]  # (ycord_start, ycord_end)
+        roi_color = frame[y:y + h, x:x + w]
 
         # recognize? deep learned model predict keras tensorflow pytorch scikit learn
         id_, conf = recognizer.predict(roi_gray)
-        if conf>=4 and conf <= 85:
+        if conf >= 4 and conf <= 85:
             # print(5: #id_)
             print(labels[id_])
             font = cv2.FONT_HERSHEY_SIMPLEX
             name = labels[id_]
             color = (255, 255, 255)
             stroke = 2
-            cv2.putText(frame, name, (x,y), font, 1, color, stroke, cv2.LINE_AA)
+            cv2.putText(frame, name, (x, y), font, 1, color, stroke, cv2.LINE_AA)
 
         img_item = "7.png"
         cv2.imwrite(img_item, roi_color)
 
-        color = (255, 0, 0) #BGR 0-255
+        color = (255, 0, 0)  # BGR 0-255
         stroke = 2
         end_cord_x = x + w
         end_cord_y = y + h
         cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)
-        #subitems = smile_cascade.detectMultiScale(roi_gray)
-        #for (ex,ey,ew,eh) in subitems:
+        # subitems = smile_cascade.detectMultiScale(roi_gray)
+        # for (ex,ey,ew,eh) in subitems:
         #	cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
     # Display the resulting frame
-    cv2.imshow('frame',frame)
+    cv2.imshow('frame', frame)
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
 
