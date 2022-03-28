c408781ee3a6c39961664c18a6eebd9bbeaf7347
Sebastian Opiyo <sebamopale@gmail.com>
2022-03-28 14:06:55 +0300

    Add db and login logic

--- visualRecognitionApp/src/data/db.py
+++ visualRecognitionApp/src/data/db.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-# database driver
+# database driver for register db
 db = sqlite3.connect('register.db')
 try:
     db_cursor = db.cursor()
@@ -16,3 +16,31 @@ try:
 except sqlite3.Error as er:
     print(f'Sqlite3 encountered an error, {er}')
     pass
+
+# userdata db
+con = sqlite3.connect('userdata.db')
+cur = con.cursor()
+cur.execute('''CREATE TABLE IF NOT EXISTS record(
+                    name text, 
+                    email text, 
+                    contact number, 
+                    gender text, 
+                    country text,
+                    password text
+                )
+            ''')
+con.commit()
+
+con = sqlite3.connect('userdata.db')
+cur = con.cursor()
+name, email, contact, gender, country, password = "Rodney", "rodney@gmail.com", "0717405766", "male", "kenya", "12345"
+cur.execute("INSERT INTO record VALUES (:name, :email, :contact, :gender, :country, :password)", {
+    'name': name,
+    'email': email,
+    'contact': contact,
+    'gender': gender,
+    'country': country,
+    'password': password
+
+})
+con.commit()
