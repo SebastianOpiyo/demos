3cf4d436a3b865d7271367f213dc18e511d076bf
Sebastian Opiyo <alwyn@Alwyns-iMac.local>
2023-01-07 23:59:02 +0300

    Add sound effects

--- pygame/flappybird/original_flappy_bird.py
+++ pygame/flappybird/original_flappy_bird.py
@@ -24,11 +24,16 @@ green = (0, 255, 0)
 START_GAME_FONT = pygame.font.SysFont('comicsansms', 50)
 END_GAME_FONT = pygame.font.SysFont('comicsansms', 50)
 
+# Sound Effects
+FLAP_SOUND = pygame.mixer.Sound(os.path.join("assets", "flap.wav"))
+CRASH_SOUND = pygame.mixer.Sound(os.path.join("assets", "sfx_die.ogg"))
+SCORE_SOUND = pygame.mixer.Sound(os.path.join("assets", "score.wav"))
+
 # Background Image
 BACKGROUND_IMAGE = pygame.image.load(os.path.join("assets", "background.png")).convert_alpha()
 
 # Image Sprites
-player1Image = pygame.image.load(os.path.join("assets", "1.png")).convert_alpha()
+# player1Image = pygame.image.load(os.path.join("assets", "1.png")).convert_alpha()
 
 # Load the sprite images for animation
 image_sprite = [
@@ -41,6 +46,7 @@ image_sprite = [
     pygame.image.load(os.path.join("assets", "1.png")).convert_alpha()
 ]
 
+
 # We use the frame variable to iterate through the image_sprite list.
 frame_value = 0
 
@@ -52,15 +58,16 @@ OBSTACLE_SURFACE = pygame.image.load(os.path.join("assets", "pipe.png"))
 BACKGROUND_IMAGE = pygame.transform.scale(BACKGROUND_IMAGE, (SCREENWIDTH, SCREENHEIGHT))
 OBSTACLE_SURFACE = pygame.transform.scale2x(OBSTACLE_SURFACE)
 player1ImageWidthHeight = [70, 90]  # set size for player sprite
+player1Image = image_sprite[frame_value]  # load player1 image into a variable
 player1Image = pygame.transform.scale(player1Image, player1ImageWidthHeight)  # modifies size of image
 player1pos = [SCREENWIDTH//2, SCREENHEIGHT//2]    #X,Y for centre
 
 # Obstacle settings
-OBSTACLE_WIDTH = 70
-obstacle_height = random.randint(150, 550)
+OBSTACLE_WIDTH = 50
+obstacle_height = random.randint((SCREENWIDTH//4), (SCREENHEIGHT-10))
 OBSTACLE_COLOR = green
-obstacle_x_pos = 300
-obstacle_x_change_pos = -4
+obstacle_x_pos = 500
+obstacle_x_change_pos = -5
 startTime = pygame.time.get_ticks()  # stores current time
 
 gameState = "running"  # controls which state the games is in
@@ -78,21 +85,18 @@ while gameState != "exit":  # game loop - note:  everything in the mainloop is i
     # To see the results properly we set the framerate to 3fps.
     # clock.tick(3)
 
-    # reset the frame to zero if the its value is greater than
+    # reset the frame to zero if its value is greater than
     # the list length.
     if frame_value > len(image_sprite):
         frame_value = 0
-    
-    # store the image in variable
-    image = image_sprite[frame_value]
 
     # draw the player
-    screen.blit(image, (player1pos[0], player1pos[1]))
+    screen.blit(player1Image, (player1pos[0], player1pos[1]))
 
     # draw the obstacles
     pygame.draw.rect(screen, OBSTACLE_COLOR, (obstacle_x_pos, 0, OBSTACLE_WIDTH, obstacle_height))
-    bottom_obstacle_height = 550 - obstacle_height - 50
-    pygame.draw.rect(screen, OBSTACLE_COLOR, (obstacle_x_pos, 550, OBSTACLE_WIDTH, bottom_obstacle_height))
+    bottom_obstacle_height = 535 - obstacle_height - 150
+    pygame.draw.rect(screen, OBSTACLE_COLOR, (obstacle_x_pos, 535, OBSTACLE_WIDTH, bottom_obstacle_height))
 
     # move the obstacles
     obstacle_x_pos += obstacle_x_change_pos
@@ -113,13 +117,15 @@ while gameState != "exit":  # game loop - note:  everything in the mainloop is i
     if keys[pygame.K_UP] or keys[pygame.K_SPACE]:
         if not player1pos[1] < 0:
             player1pos[1] -= 10
+            FLAP_SOUND.play()
+    frame_value += 1
 
-    # frame_value += 1
     # detect a collision
     if 50 <= obstacle_x_pos <= (100 + 70):
         if player1pos[1] <= obstacle_height or player1pos[1] >= (bottom_obstacle_height - 70):
             display_content = END_GAME_FONT.render("GAME OVER!", True, red)
-            screen.blit(display_content, (20, 100))
+            screen.blit(display_content, (250, 150))
+            CRASH_SOUND.play()
             # pygame.quit()
 
     # your code ends here ###############################
