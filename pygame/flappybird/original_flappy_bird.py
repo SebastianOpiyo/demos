2d3431790936cf11f5f056e379ba603e2c270d1d
Sebastian Opiyo <alwyn@Alwyns-iMac.local>
2023-01-08 18:00:53 +0300

    WIP-Animation

--- pygame/flappybird/original_flappy_bird.py
+++ pygame/flappybird/original_flappy_bird.py
@@ -31,6 +31,7 @@ SCORE_SOUND = pygame.mixer.Sound(os.path.join("assets", "score.wav"))
 
 # Background Image
 BACKGROUND_IMAGE = pygame.image.load(os.path.join("assets", "background.png")).convert_alpha()
+BASE = pygame.image.load(os.path.join("assets", "base.png")).convert_alpha()
 
 # Image Sprites
 # player1Image = pygame.image.load(os.path.join("assets", "1.png")).convert_alpha()
@@ -42,13 +43,15 @@ image_sprite = [
     pygame.image.load(os.path.join("assets", "3.png")).convert_alpha(),
     pygame.image.load(os.path.join("assets", "4.png")).convert_alpha(),
     pygame.image.load(os.path.join("assets", "5.png")).convert_alpha(),
-    pygame.image.load(os.path.join("assets", "6.png")).convert_alpha(),
-    pygame.image.load(os.path.join("assets", "1.png")).convert_alpha()
+    pygame.image.load(os.path.join("assets", "6.png")).convert_alpha()
 ]
 
 
 # We use the frame variable to iterate through the image_sprite list.
 frame_value = 0
+# base scroll variables
+base_scroll = 0
+scroll_speed = 4
 
 # Obstacle Image
 OBSTACLE_SURFACE = pygame.image.load(os.path.join("assets", "pipe.png"))
@@ -56,46 +59,76 @@ OBSTACLE_SURFACE = pygame.image.load(os.path.join("assets", "pipe.png"))
 
 # resize images
 BACKGROUND_IMAGE = pygame.transform.scale(BACKGROUND_IMAGE, (SCREENWIDTH, SCREENHEIGHT))
+BASE = pygame.transform.scale(BASE, (SCREENWIDTH, SCREENHEIGHT))
 OBSTACLE_SURFACE = pygame.transform.scale2x(OBSTACLE_SURFACE)
 player1ImageWidthHeight = [70, 90]  # set size for player sprite
-player1Image = image_sprite[frame_value]  # load player1 image into a variable
-player1Image = pygame.transform.scale(player1Image, player1ImageWidthHeight)  # modifies size of image
 player1pos = [SCREENWIDTH//2, SCREENHEIGHT//2]    #X,Y for centre
 
 # Obstacle settings
+OBSTACLE_GAP = 150
+OBSTACLE_FREQUENCY = 1450  # Milliseconds
+LAST_OBSTACLE = pygame.time.get_ticks() - OBSTACLE_FREQUENCY
 OBSTACLE_WIDTH = 50
 obstacle_height = random.randint((SCREENWIDTH//4), (SCREENHEIGHT-10))
 OBSTACLE_COLOR = green
-obstacle_x_pos = 500
+obstacle_x_pos = 700
 obstacle_x_change_pos = -5
 startTime = pygame.time.get_ticks()  # stores current time
 
+moving = False  # check if the bord is moving
 gameState = "running"  # controls which state the games is in
 # game loop #################### runs 60 times a second!
 while gameState != "exit":  # game loop - note:  everything in the mainloop is indented one tab
 
-    for event in pygame.event.get():  # get user interaction events
-        if event.type == pygame.QUIT:  # tests if window's X (close) has been clicked
-            gameState = "exit"  # causes exit of game loop
     # your code starts here ##############################
+    clock.tick(FPS)  # setting game fps
     screen.fill(black)
     # Add the background
     screen.blit(BACKGROUND_IMAGE, (0, 0))
 
-    # To see the results properly we set the framerate to 3fps.
-    # clock.tick(3)
+    for event in pygame.event.get():  # get user interaction events
+        if event.type == pygame.QUIT:  # tests if window's X (close) has been clicked
+            gameState = "exit"  # causes exit of game loop
+            sys.exit()
+
+    # Storing keys pressed in a new variable using key.get_pressed() method
+    keys = pygame.key.get_pressed()
+
+    # Changing the y-coordinate of the player and setting moving variable to true
+    if keys[K_UP] or keys[K_SPACE]:
+        if not player1pos[1] < 0:
+            player1pos[1] -= 10
+            moving = True
+            FLAP_SOUND.play()
+
+    if keys[KEYDOWN]:
+        player1pos[1] += 10
+        moving = True
+
+    if moving:
+        frame_value += 1
+        # screen.blit(player1Image, (player1pos[0], player1pos[1]))
 
     # reset the frame to zero if its value is greater than
     # the list length.
     if frame_value > len(image_sprite):
         frame_value = 0
 
+    # Add scrolling effect to the base
+    # screen.blit(BASE, (base_scroll, 768))
+    # base_scroll -= scroll_speed
+    # if abs(base_scroll) > 70:
+    #     base_scroll = 0
+
     # draw the player
+    player1Image = image_sprite[frame_value]  # load player1 image into a variable
+    player1Image = pygame.transform.scale(player1Image, player1ImageWidthHeight)  # modifies size/scale of the image
     screen.blit(player1Image, (player1pos[0], player1pos[1]))
+    pygame.display.update()  # Update the surface
 
     # draw the obstacles
     pygame.draw.rect(screen, OBSTACLE_COLOR, (obstacle_x_pos, 0, OBSTACLE_WIDTH, obstacle_height))
-    bottom_obstacle_height = 535 - obstacle_height - 150
+    bottom_obstacle_height = 535 - obstacle_height - 150//2
     pygame.draw.rect(screen, OBSTACLE_COLOR, (obstacle_x_pos, 535, OBSTACLE_WIDTH, bottom_obstacle_height))
 
     # move the obstacles
@@ -103,22 +136,15 @@ while gameState != "exit":  # game loop - note:  everything in the mainloop is i
 
     # generate new obstacle
     if obstacle_x_pos <= -10:
-        obstacle_x_pos = 600
+        obstacle_x_pos = 200
         obstacle_height = random.randint(150, 550)
 
     pygame.draw.rect(screen, OBSTACLE_COLOR, (obstacle_x_pos, 0, OBSTACLE_WIDTH, obstacle_height))
-    bottom_obstacle_height = 550 - obstacle_height - 50
-    pygame.draw.rect(screen, OBSTACLE_COLOR, (obstacle_x_pos, 550, OBSTACLE_WIDTH, bottom_obstacle_height))
+    bottom_obstacle_height = 535 - obstacle_height - 150//2
+    pygame.draw.rect(screen, OBSTACLE_COLOR, (obstacle_x_pos, 535, OBSTACLE_WIDTH, bottom_obstacle_height))
 
     # move the player
-    player1pos[1] += 5
-    keys=pygame.key.get_pressed()
-
-    if keys[pygame.K_UP] or keys[pygame.K_SPACE]:
-        if not player1pos[1] < 0:
-            player1pos[1] -= 10
-            FLAP_SOUND.play()
-    frame_value += 1
+    # player1pos[1] += 5
 
     # detect a collision
     if 50 <= obstacle_x_pos <= (100 + 70):
@@ -130,10 +156,11 @@ while gameState != "exit":  # game loop - note:  everything in the mainloop is i
 
     # your code ends here ###############################
     pygame.display.flip()  # transfers build screen to human visible screen
-    clock.tick(FPS)  # limits game to frame per second, FPS value
+    # clock.tick(FPS)  # limits game to frame per second, FPS value
     
 
 # out of game loop ###############
+
 print("The game has closed")  # notifies user the game has ended
 pygame.quit()   # stops the game engine
 sys.exit()  # close operating system window
